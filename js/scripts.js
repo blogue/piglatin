4fe5d6a9f573532219958e179e3cb40255d4a6a4
Ben Logue <benjamin.logue73@gmail.com>
2016-06-25 21:47:08 -0700

    refactor functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,66 +3,33 @@ var sentenceSplitter = function(sentence) {
     var sentenceSplit = sentence.split(" ");
     var loopLimit = sentenceSplit.length;
     for (i = 0; i < loopLimit; i += 1) {
-      // console.log(evaluateText(sentenceSplit[i]));
-      // console.log(sentenceSplit[i]);
-      // debugger;
-      piglatinSentence.push(evaluateText(sentenceSplit[i]));
+        if (!sentenceSplit[i]) { continue; } // Catch the case of '' (empty string) resulting from two consecutive spaces in user input
+        piglatinSentence.push(evaluateText(sentenceSplit[i]));
     }
-    console.log(piglatinSentence);
-    return sentenceJoiner(piglatinSentence);
-}
-var sentenceJoiner = function(wordJoin) {
-  finalSentence = "";
-  for (i = 0; i < wordJoin.length; i++) {
-    // wordJoin[i].join(" ")
-    var joinedWord = wordJoin[i].join('');
-    joinedWord += " ";
-    finalSentence += joinedWord;
-  }
-  return finalSentence;
-}
-
-
-
-
-var evaluateText = function(input) {
-  inputArr = input.split("");
-
-  if (inputArr[0] === "q" && inputArr[1] === "u") {
-    inputArr.splice(0, 2);
-    inputArr.push("qu");
-    inputArr.push("ay")
-    return inputArr;
-
-  }
-  if (inputArr[0] !== "a" && inputArr[0] !== "e" && inputArr[0] !== "i" && inputArr[0] !== "o" && inputArr[0] !== "u" &&  inputArr.length <= 3 ) {
-    inputArr.push(inputArr.shift());
-    inputArr.push('ay');
-    return inputArr;
-  }
-  if (inputArr[0] === "a" || inputArr[0] === "e" || inputArr[0] === "i" || inputArr[0] === "o" || inputArr[0] === "u") {
-    inputArr.push('ay')
-    return inputArr;
-  }
-
-  for (j=0; j<=1; j++) {
-   if (inputArr[0] !== "a" && inputArr[0] !== "e" && inputArr[0] !== "i" && inputArr[0] !== "o" && inputArr[0] !== "u") {
-     inputArr.push(inputArr.shift());
-   }
- }
-  inputArr.push('ay');
-  console.log(inputArr)
-  return inputArr;
+    return piglatinSentence.join(' ');
 };
 
+var evaluateText = function(input) {
+    var inputArr = input.split(""),
+        vowels = ['a', 'e', 'i', 'o', 'u'],
+        len = inputArr.length,
+        lastLetter = '',
+        output, i;
+  for (i = 0; i < len; i++) {
+    if (vowels.indexOf(inputArr[0]) !== -1 && !(lastLetter === 'q' && inputArr[0] === 'u')) { break; }
+    lastLetter = inputArr.shift();
+    inputArr.push(lastLetter);
+  }
 
+    inputArr.push('ay');
+    output = inputArr.join('');
+    console.log('Word \'' + input + '\' => \'' + output + '\'');
+    return output;
+};
 
 $(document).ready(function() {
   $("#userInput").submit(function(event){
     var sentenceToTranslate = $("#userSentence").val().toLowerCase();
-
-
-    // $(".output").append(evaluateText(sentenceToTranslate));
     $(".output h1").text(sentenceSplitter(sentenceToTranslate));
     event.preventDefault();
   });
